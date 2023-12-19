edc1797a89946d8082596d7711db96cf3d65244f
chromatices <pjh403@naver.com>
2023-12-19 10:12:18 +0900

    [Fix] Remove unnecessary features

--- src/views/Starter.js
+++ src/views/Starter.js
@@ -1,17 +1,8 @@
 import { Col, Row } from "reactstrap";
-// import PipelineTable from "../components/dashboard/WorkflowTable";
 import WorkflowInfo from "../components/dashboard/NewWorkflowTable";
-// import ArgoComponent from "./ui/get_argo_status";
 import { Card, CardBody, CardSubtitle} from "reactstrap";
 
 const Starter = () => {
-  // const ArgoWorkflowComponent = () => {
-  //   return (
-  //     <div>
-  //       <iframe src="https://10.0.1.102:30100/workflows" width="100%" height="600" />
-  //     </div>
-  //   );
-  // };
 
   return (
     <div>
